eac63bccc3b37992a60cfa34ca7f771414f56ef9
shinSnap <xshijimax@gmail.com>
2019-07-12 21:03:39 -0700

    refactor

--- createp.py
+++ createp.py
@@ -1,67 +1,67 @@
 #!/usr/bin/env python3
 import requests
-import json
 from sys import argv
 from platform import mac_ver
 from os import environ, path, mkdir, chdir, system
 
 # Set Variables
-projectname = argv[1]
 authorization_token = environ.get('GITHUBAPI')
+
+
+class GitHub:
+
+    def __init__(self, authorization_token, project_name=None):
+        self._URI = 'https://api.github.com/user/repos'
+        self._token = authorization_token
+        self._project_name = project_name
+
+    def create_repo(self, private=False, issues=True, projects=True,
+                    wiki=True, license_template='mit'):
+
+        header = {
+            'Authorization': 'token ' + self._token,
+        }
+        parameters = {
+            'name': self._project_name,
+            'private': private,
+            'has_issues': issues,
+            'has_projects': projects,
+            'has_wiki': wiki,
+            'license_template': license_template,
+            'auto_init': True
+        }
+
+        response = requests.post(
+            self._URI,
+            headers=header,
+            json=parameters
+        )
+
+        if response.status_code == 201:
+            print('Repo Created')
+            data = response.json()['ssh_url']
+            return data
+        else:
+            print('...Error creating repo...')
+            print(response.content)
+            exit()
 
 
 def createprojectfolder(projectname):
     home = path.expanduser('~')
-    workdir = home + '/git/Personal/' + projectname
+    workdir = home + '/git/dev/' + projectname
     mkdir(workdir)
     chdir(workdir)
 
 
-def createreadme():
-    open('README.md', 'a').close()
-
-
 def initializegit():
     system('git init')
-    system('git add README.md')
+    # system('git add README.md')
     system('git commit -m "initial commit"')
 
 
-def githubremote(githuburl):
-    system('git remote add origin ' + githuburl)
-
-
-def githubpush():
-    system('git push -u origin master')
-
-
-def githubapi(projectname, authorization_token):
-
-    uri = 'https://api.github.com/user/repos'
-
-    header = {
-        'Authorization': 'token ' + authorization_token,
-    }
-    parameters = {
-        'name': projectname,
-        'private': True,
-        'has_issues': True,
-        'has_projects': True,
-        'has_wiki': True
-    }
-
-    response = requests.post(
-        uri,
-        params=json.dumps(parameters),
-        headers=header
-    )
-    if response.status_code == 201:
-        print('Successfully created Repo')
-        data = json.loads(response.content)
-        return data['ssh_url']
-    else:
-        print('...Error creating repo...')
-        exit()
+def createreadme():
+    open('README.md', 'a').close()
 
 
 def openvscode():
@@ -77,17 +77,25 @@ def checkos():
         exit()
 
 
-if checkos() is True:
-    try:
-        createprojectfolder(projectname)
-        createreadme()
-        initializegit()
-        repourl = githubapi(projectname, authorization_token)
-        githubremote(repourl)
-        githubpush()
-        openvscode()
-    except FileExistsError:
-        print('Project exists. Choose another name.')
-        exit()
-else:
-    print('Error running script')
+if __name__ == '__main__':
+    if checkos() is True:
+        try:
+            projectname = argv[1]
+            createprojectfolder(projectname)
+            initializegit()
+            github = GitHub(authorization_token, projectname)
+            repourl = github.create_repo(private=True)
+            print(repourl)
+            system('git remote add origin {}'.format(repourl))
+            system('git pull origin master')
+            openvscode()
+        except FileExistsError:
+            print('Project exists. Choose another name.')
+            exit()
+        except IndexError:
+            print('Please state project name when executing command')
+            print('Usage: create <projectname>')
+            print('Example: create HelloWorld')
+            exit()
+    else:
+        print('Error running script')
